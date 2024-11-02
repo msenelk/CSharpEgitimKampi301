07463ae3f7a0da15cbf82fd60beeb461355c384c
Muhammed Kizilirmak <senel_1996@hotmail.com>
2024-11-02 20:48:42 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        // Entity => Varlık demek
+        // Sadece Category tablosuna ait değerleri tutacak.
+
+        // Class --> Table
+        // Property --> Column (sütun)
+
+        // Access Modifiers
+        // Public => Her yerden erişim sağlanır.
+        // Private => Sadece bulunduğu alandan erişim sağlar.
+        // Internal => Sadece bulunduğu katmandan erişim sağlar.
+        // Protected => Bir sınıf ve o sınıfı miras alan yerlerden erişim sağlar.
+
+        // Code First Yaklaşımı
+        // Veritabanının model ve yapılandırmasının doğrudan C# kodu ile tanımlanmasını sağlar
+
+        // Field    => Classın/sınıfın içinde tanımlanan değişkenler
+        // Variable => Metod içinde tanımlanırsa değişken..
+        // Property => Değişken yapısını sonuna get ve set diye değer alıyorsa property
+
+        // prop tab yaparak yazıyoruz.
+
+        public int CategoryId { get; set; }
+        // Code Firs yaklaşımı içinde bunun birinci anahtar olduğunu göstermek için sonuna Id yazılmalı
+        public string CategoryName { get; set; }
+        // get => almak
+        // set => ayarlamak
+        public bool CategoryStatus { get; set; }
+
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerDistrict { get; set; }
+        public string CustomerCity { get; set; }
+
+        // SOLID
+
+        // Single Responsibility => Tek sorumluluk ilkesi
+        // Bir method sadece bir işi yapar.
+
+        // Open/Closed Principle (OCP) => Genişlemeye açık ancak değişikliğe kapalı olmak
+
+        // Liskov Substitution Principle (LSP) => Türetilmiş sınıflar, temel sınıflarının yerine kullanılabilir olmalıdır. Yani, bir alt sınıf, üst sınıfın yerine geçtiğinde davranışı bozmamalıdır.
+
+        // Interface Segregation Principle (ISP) => Bir sınıf, kullanmadığı yöntemleri içeren bir arayüze bağlı olmamalıdır. Arayüzler olabildiğince spesifik olmalı ve gereksiz metotlar içermemelidir.
+
+        // Dependency Inversion Principle (DIP) => Yüksek seviyeli modüller (iş mantığı) düşük seviyeli modüllere (veri erişim katmanı gibi) bağımlı olmamalıdır. Her ikisi de soyutlamalara (arayüzler veya soyut sınıflar) bağımlı olmalıdır.
+
+        //SOLID Prensiplerinin Faydaları
+            //Bakım Kolaylığı: Kod daha modüler olduğu için daha kolay anlaşılır ve değiştirilebilir.
+            //Genişletilebilirlik: Yeni özellikler eklemek, mevcut kodu değiştirmeden mümkün hale gelir.
+            //Test Edilebilirlik: Kodun test edilmesi daha kolaydır, çünkü her modül belirli bir sorumluluğa sahiptir.
+            //Esneklik: Kod, değişikliklere ve büyümeye karşı daha dayanıklı hale gelir.
+
+    }
+}
