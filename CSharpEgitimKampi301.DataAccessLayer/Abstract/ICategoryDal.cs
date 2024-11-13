bc3e1b92e3fc16a684a283ed7110d995fb1c5456
Muhammed Kizilirmak <senel_1996@hotmail.com>
2024-11-13 15:13:20 +0300

    "Ders 13 - OOP Modülü: Migration İşlemleri ve Abstract Interfaceler"

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,8 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IGenericDal<Category>
+    {
+    }
+}
---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class // Dışarıdan T değeri alacaksın ve o T değeri sadece Class olacak
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T id);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
